780986f1e04afce6f67f72f9d1eb60a808df2b79
Gopalakrishna586 <gopalakrishnathota586@gmail.com>
2019-08-30 14:07:34 +0530

    my raptor uploads

---
+++ prime numbers in a given range with count.cs
@@ -0,0 +1,57 @@
+using System;
+using System.IO;
+using System.Text;
+
+namespace prime numbers in a given range with count
+{
+   public class main_class
+   {
+      static System.Random random_generator = new System.Random();
+      public static void Main(string[] args)
+      {
+         string raptor_prompt_variable_zzyz;
+         ?? b;
+         ?? c;
+         ?? i;
+         ?? a;
+         ?? f;
+      
+         raptor_prompt_variable_zzyz ="enter lower limit";
+         Console.WriteLine(raptor_prompt_variable_zzyz);
+         a= Double.Parse(Console.ReadLine());
+         f =0;
+         raptor_prompt_variable_zzyz ="enter upper limit";
+         Console.WriteLine(raptor_prompt_variable_zzyz);
+         b= Double.Parse(Console.ReadLine());
+         c =0;
+         while (1)
+         {
+            i =1;
+            if (a>b)) break;
+            while (!(i>a))
+            {
+               if (a % i==0)
+               {
+                  f =f+1;
+               }
+               else
+               {
+               }
+               i =i+1;
+            }
+            if (f==2)
+            {
+               Console.WriteLine(" "+a);
+               c =c+1;
+            }
+            else
+            {
+            }
+            f =0;
+            i =i+1;
+            a =a+1;
+         }
+         Console.WriteLine("count"+c);
+      }
+   }
+}
